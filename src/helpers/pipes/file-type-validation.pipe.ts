5ec212e7752acfa1330bc8597c358b1fd7b614aa
iqballm09 <22_iqbal@apps.ipb.ac.id>
2024-02-06 20:46:01 +0700

    fix: update name of file type validation

--- src/helpers/pipes/file-type-validation.pipe.ts
+++ src/helpers/pipes/file-type-validation.pipe.ts
@@ -1,7 +1,7 @@
 import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 
 @Injectable()
-export class FileSizeValidationPipe implements PipeTransform {
+export class FileTypeValidationPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     const typeFile = 'pdf';
     return value.mimetype == typeFile;
