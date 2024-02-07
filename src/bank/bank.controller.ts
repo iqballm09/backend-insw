15f90c99d406cf777d6d081ebc560f947ccf520a
iqballm09 <22_iqbal@apps.ipb.ac.id>
2024-02-07 19:41:29 +0700

    fix: delete bank entity

--- src/bank/bank.controller.ts
+++ src/bank/bank.controller.ts
@@ -1,19 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Req,
-} from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { BankService } from './bank.service';
-import { CreateBankDto } from './dto/create-bank.dto';
-import { UpdateBankDto } from './dto/update-bank.dto';
-import { OidcGuard } from 'src/auth/guard/oidc.guard';
-import { Request } from 'express';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('bank')
