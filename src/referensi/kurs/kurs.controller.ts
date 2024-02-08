60469b2276cb4b434a320634d5846dce09396397
iqballm09 <22_iqbal@apps.ipb.ac.id>
2024-02-08 17:33:27 +0700

    feat: add kabkota, kurs, negara, pelabuhan, shippingline, size typecode, status do, terminal operator referensi to swagger ui

--- src/referensi/kurs/kurs.controller.ts
+++ src/referensi/kurs/kurs.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { KursService } from './kurs.service';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('kurs')
+@ApiTags('Data Referensi')
 export class KursController {
   constructor(private readonly kursService: KursService) {}
 
   @Get()
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
   findAll(@Req() req: any) {
     return this.kursService.findAll(req.token);
--- src/referensi/terminal-operator/terminal-operator.controller.ts
+++ src/referensi/terminal-operator/terminal-operator.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { TerminalOperatorService } from './terminal-operator.service';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('terminal-operator')
+@ApiTags('Data Referensi')
 export class TerminalOperatorController {
   constructor(private readonly terminalOpService: TerminalOperatorService) {}
 
   @Get()
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
   findAll(@Req() req: any) {
     return this.terminalOpService.findAll(req.token);
