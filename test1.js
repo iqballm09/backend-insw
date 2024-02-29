b60287fea0b38600f3cfab9a010f8eacd1daf42a
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2024-02-29 11:21:29 +0000

    fix: delete test1.js file

--- test1.js
+++
@@ -1,6 +0,0 @@
-// Assume you have retrieved the user's timezone (e.g., "America/New_York")
-const moment = require('moment-timezone')
-const userTimezone = "Asia/Jakarta";
-
-const currentDate = moment(new Date()).tz(userTimezone).format('DD-MM-YYYY HH:mm:ss')
-console.log(currentDate)
